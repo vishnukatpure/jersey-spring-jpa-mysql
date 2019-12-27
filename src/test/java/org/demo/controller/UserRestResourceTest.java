c3583ab6d3a839e5886bff1aaf7c95e931da205a
Amol Shinde <ashinde-v@hypergrid.com>
2019-12-27 17:43:20 +0530

    test cae modify

--- src/test/java/org/demo/controller/UserRestResourceTest.java
+++ src/test/java/org/demo/controller/UserRestResourceTest.java
@@ -1,15 +1,13 @@
 package org.demo.controller;
 
-import org.demo.rest.modal.UserModal;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRestResourceTest {
 
 	@Test
-	public UserModal getUser() {
+	public void getUser() {
 		boolean value = true;
 		Assert.assertEquals(true, value);
-		return null;
 	}
 }
