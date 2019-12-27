1545c59712d107c3a96d0d98acda21cf6a49e3be
Amol Shinde <ashinde-v@hypergrid.com>
2019-12-27 17:40:15 +0530

    add test case

---
+++ src/test/java/org/demo/controller/UserRestResourceTest.java
@@ -0,0 +1,15 @@
+package org.demo.controller;
+
+import org.demo.rest.modal.UserModal;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRestResourceTest {
+
+	@Test
+	public UserModal getUser() {
+		boolean value = true;
+		Assert.assertEquals(true, value);
+		return null;
+	}
+}
